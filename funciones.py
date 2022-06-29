1d4974c890f630ebed6a313a0fb6fc05983c2e8f
Pep3 <pep3.marquez@gmail.com>
2022-06-29 01:17:20 -0400

    agregando ventana para agregar matricula

--- funciones.py
+++ funciones.py
@@ -10,11 +10,11 @@ def init_multilist_alumnos(frame_container):
         'Otros datos',
     ]
     
-    ### revisar por que me esta dando NONE get all alumnos
     lista_alumnos = get_all_alumnos()
-    print(lista_alumnos)
     if not lista_alumnos:
         lista_alumnos = [('','','','','')]
     
     multi = MultiColumnListbox(frame_container, header_alumnos,lista_alumnos)
     return multi
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from tkinter import Button, Frame, Tk
-
+from tkinter import Button, Frame, Tk, Toplevel
+from db_handler import get_municipios
 from funciones import init_multilist_alumnos
+from tkform_new_matr import Form_new_matric
 
+# Funciones #
+
+
+def agregar_matricula():
+    top = Toplevel()
+    top.title('Agregando matricula')
+    top.attributes('-topmost', 'true')
+
+    municipios = get_municipios()
+
+    form = Form_new_matric(top, municipios)
+    top.mainloop()
+    
+    print(form.guardar)
+
+
+# VISUAL #
 
 root = Tk()
 root.title('Transito matriculas')
@@ -13,9 +31,10 @@ body_frame.pack(expand=True, fill='both')
 buttons_frame = Frame(body_frame, width=150, background='green')
 buttons_frame.pack(fill='y', side='left')
 
-bt_new_matr = Button(buttons_frame, text='Nueva matricula')
+bt_new_matr = Button(buttons_frame,
+                     text='Nueva matricula',
+                     command=agregar_matricula)
 bt_new_matr.pack()
-
 
 tree_frame = Frame(body_frame, background='blue')
 tree_frame.pack(expand=True, fill='both', side='right')
