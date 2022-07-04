87e588f912fc6fd5f155810cbfc763d449db5156
Pep3 <pep3.marquez@gmail.com>
2022-07-03 23:59:48 -0400

    instruccion para que pyinstaller acepte tkcalendar

--- instruccion_compilation.py
+++ instruccion_compilation.py
@@ -1,6 +1,7 @@
 import os
 
 
-instr = 'pyinstaller.exe --windowed --onefile --icon=assets/imgs/logo.ico --name=Matriculas main.py'
+instr = 'pyinstaller.exe --windowed --onefile --icon=assets/imgs/logo.ico --name=Matriculas --hidden-import babel.numbers main.py'
 
-os.system(instr)
+#os.system(instr)
+os.popen(instr)
